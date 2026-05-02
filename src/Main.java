796153dbe6e25278a1c35b7eb36faddd6cb6cf84
aman qais <amanqaismunim@gmail.com>
2026-05-02 15:03:38 -0400

    person 3 stats

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import service.GameService;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        GameService service = new GameService();
+
+        try {
+            service.addGame(1, "Valorant");
+            service.addStats(1, 10, 5, 200);
+            service.updateStats(1, 15, 6, 250);
+            service.deleteStats(1);
+            System.out.println("Everything is good");
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
