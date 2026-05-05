4589243e210c85c164bfa2fc55b8a594b58929e9
Armaan Arora <armaanarora880@gmail.com>
2026-05-05 00:49:04 -0400

    Adding password for the User

--- src/main/java/org/example/javafxui/model/User.java
+++ src/main/java/org/example/javafxui/model/User.java
@@ -4,10 +4,12 @@ public class User {
     public int id;
     public String username;
     public String email;
+    public String password;
 
-    public User(int id, String username, String email) {
+    public User(int id, String username, String email, String password) {
         this.id = id;
         this.username = username;
         this.email = email;
+        this.password = password;
     }
 }
